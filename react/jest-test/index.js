210a79c5a513e76c8ea2f976130f18c808be70f6
YoungJoonNam <nyjoon2022@gmail.com>
2022-03-15 16:11:59 +0900

    react study

---
+++ react/jest-test/index.js
@@ -0,0 +1,22 @@
+const sum = (a, b) => {
+    return a + b;
+};
+
+const person = (name, age) => {
+
+    return {
+
+        name,
+        age,
+
+
+    }
+
+
+};
+
+module.exports = {
+    sum,
+    person,
+};
+
